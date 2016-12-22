bcfc667abef64b4cda7250d102b3e1e31532d228
Riad Benguella <benguella@gmail.com>
2016-12-22 10:59:32 +0100

    Trying a new approach

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -39,8 +39,8 @@ import AsyncLoad from 'components/async-load';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { setEditorLastDraft, resetEditorLastDraft } from 'state/ui/editor/last-draft/actions';
 import { isEditorDraftsVisible, getEditorPostId, getEditorPath } from 'state/ui/editor/selectors';
-import { toggleEditorDraftsVisible, setEditorPostId } from 'state/ui/editor/actions';
-import { receivePost, resetPostEdits } from 'state/posts/actions';
+import { toggleEditorDraftsVisible } from 'state/ui/editor/actions';
+import { receivePost, savePostSuccess } from 'state/posts/actions';
 import { getPostEdits, isEditedPostDirty } from 'state/posts/selectors';
 import EditorSidebarHeader from 'post-editor/editor-sidebar/header';
 import EditorDocumentHead from 'post-editor/editor-document-head';
@@ -139,9 +139,6 @@ export const PostEditor = React.createClass( {
 
 	componentWillUnmount: function() {
 		PostEditStore.removeListener( 'change', this.onEditedPostChange );
-
-		// Reset post edits after leaving editor
-		this.props.resetPostEdits( this.props.siteId, this.props.postId );
 
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 		actions.stopEditing();
@@ -695,18 +692,11 @@ export const PostEditor = React.createClass( {
 			this.props.resetEditorLastDraft();
 		}
 
-		// Assign editor post ID to saved value (especially important when
-		// transitioning from an unsaved post to a saved one)
-		if ( post.ID !== this.props.postId ) {
-			this.props.setEditorPostId( post.ID );
-		}
+		// Remove this when the editor is completely reduxified ( When using Redux actions for all post saving requests )
+		this.props.savePostSuccess( post.site_ID, this.props.postId, post, {} );
 
 		// Receive updated post into state
 		this.props.receivePost( post );
-
-		// Reset previous edits, preserving type
-		this.props.resetPostEdits( this.props.siteId );
-		this.props.resetPostEdits( post.site_ID, post.ID );
 
 		const nextState = {
 			isSaving: false,
@@ -790,8 +780,7 @@ export default connect(
 			setEditorLastDraft,
 			resetEditorLastDraft,
 			receivePost,
-			resetPostEdits,
-			setEditorPostId,
+			savePostSuccess,
 			setEditorModePreference: savePreference.bind( null, 'editor-mode' ),
 			setLayoutFocus,
 		}, dispatch );
